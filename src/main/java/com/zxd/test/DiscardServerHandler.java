fb23587f7bfcf12f43028b2735415ce4aaf1d8af
xiaodong.zou <zxd_0825@163.com>
2019-10-15 23:19:36 +0800

    discard server handler

---
+++ src/main/java/com/zxd/test/DiscardServerHandler.java
@@ -0,0 +1,21 @@
+package com.zxd.test;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("discard...........");
+        ((ByteBuf)msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
