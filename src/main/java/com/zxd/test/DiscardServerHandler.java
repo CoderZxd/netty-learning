3f717995e6ab2a0e2eb7ed36daaa2a2a54a03c9e
xiaodong.zou <zxd_0825@163.com>
2019-10-20 21:36:55 +0800

    test

--- src/main/java/com/zxd/test/DiscardServerHandler.java
+++ src/main/java/com/zxd/test/DiscardServerHandler.java
@@ -3,12 +3,25 @@ package com.zxd.test;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 
 /**
  * ChannelInboundHandlerAdapter类提供了可以覆盖各种事件的方法，我们只需要继承这个类，并覆盖其中的方法即可
  */
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+        System.out.println("channelActive................");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        super.channelInactive(ctx);
+        System.out.println("channelInactive................");
+    }
 
     /**
      * 覆盖事件处理的方法，每当从客户端接收到新数据时，都会调用此方法
@@ -17,8 +30,19 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("discard...........");
-        ((ByteBuf)msg).release();
+        System.out.println("show message...........");
+        ByteBuf in = (ByteBuf) msg;
+        try{
+            System.out.println(in.toString(io.netty.util.CharsetUtil.UTF_8));
+//            while (in.isReadable()){
+//                System.out.println((char) in.readByte());
+//                System.out.flush();
+//            }
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
 
     /**
