bfd40875731ddb63f976de54e00744727a6f9741
xiaodong.zou <zxd_0825@163.com>
2019-10-20 21:13:10 +0800

    add comment

--- src/main/java/com/zxd/test/DiscardServer.java
+++ src/main/java/com/zxd/test/DiscardServer.java
@@ -18,19 +18,31 @@ public class DiscardServer {
     }
 
     public void run() throws Exception{
+        // NioEventLoopGroup是一个处理I/O操作的多线程事件循环
+        // 使用了多少个线程以及它们如何映射到创建的Channels取决于EventLoopGroup实现，甚至可以通过构造函数进行配置
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try{
+            // 设置服务器的帮助程序类
             ServerBootstrap bootstrap = new ServerBootstrap();
+            // 将创建好的两个group添加到serverBootStrap中
             bootstrap.group(bossGroup,workerGroup)
+                    // 指定使用 NioServerSocketChannel 来接受传入连接
+                    // childHandler指定处理程序，ChannelInitializer旨在帮助用户配置新的channel
                     .channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
-                @Override
-                protected void initChannel(SocketChannel socketChannel) throws Exception {
-                    socketChannel.pipeline().addLast(new DiscardServerHandler());
-                }
-            }).option(ChannelOption.SO_BACKLOG,128).childOption(ChannelOption.SO_KEEPALIVE,true);
-
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            //将我们定义好的DiscardServerHandler丢在管道中
+                            socketChannel.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    })
+                    // 这里可以设置特定的channel参数，为了接受传入的连接
+                    .option(ChannelOption.SO_BACKLOG,128)
+                    // 追加操作项，因为我们正在编写TCP/IP服务器，因此我们可以设置套接字选项，如tcpNoDelay和keepAlive
+                    .childOption(ChannelOption.SO_KEEPALIVE,true);
+            // 绑定端口号并接收传入的连接
             ChannelFuture f = bootstrap.bind(port).sync();
+            // 关闭服务器
             f.channel().closeFuture().sync();
         }finally {
             workerGroup.shutdownGracefully();
--- src/main/java/com/zxd/test/DiscardServerHandler.java
+++ src/main/java/com/zxd/test/DiscardServerHandler.java
@@ -5,14 +5,27 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 
+/**
+ * ChannelInboundHandlerAdapter类提供了可以覆盖各种事件的方法，我们只需要继承这个类，并覆盖其中的方法即可
+ */
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 
+    /**
+     * 覆盖事件处理的方法，每当从客户端接收到新数据时，都会调用此方法
+     * @param ctx
+     * @param msg
+     */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         System.out.println("discard...........");
         ((ByteBuf)msg).release();
     }
 
+    /**
+     * 处理事件引发的异常，大多数情况下，应该记录捕获的异常并在此关闭其关联的同道
+     * @param ctx
+     * @param cause
+     */
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
